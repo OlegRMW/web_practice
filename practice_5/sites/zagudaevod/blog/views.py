daff7c745a8925d1c85252b35ea3ab74fd265094
OlegRMW <zagudpev@mail.ru>
2025-10-02 23:52:37 +0300

    5 practice done

--- practice_5/sites/zagudaevod/blog/views.py
+++ practice_5/sites/zagudaevod/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'blog/Praktikum_5.html', {'title': 'lists'})
+    return render(request, 'blog/Praktikum5.html', {'title': 'lists'})
 
 
 
