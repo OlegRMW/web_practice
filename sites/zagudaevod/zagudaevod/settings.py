1aee06c8155ae3d4a74dd55cd504c7ef87d91cbb
OlegRMW <zagudpev@mail.ru>
2025-11-22 11:53:22 +0300

    Commit local changes before merge

--- sites/zagudaevod/zagudaevod/settings.py
+++ sites/zagudaevod/zagudaevod/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig",
-    "django_extensions"
+    "blog.apps.BlogConfig"
 ]
 
 MIDDLEWARE = [
